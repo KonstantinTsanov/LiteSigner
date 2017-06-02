0717e742dc3b69cb233981aca0ec4d2768739782
Konstantin Tsanov <k.tsanov@gmail.com>
2017-06-02 18:40:43 +0300

    Better implementation of the searching algorithm. Still sometimes freezes while searching.

--- src/main/java/tools/DeviceManager.java
+++ src/main/java/tools/DeviceManager.java
@@ -9,14 +9,14 @@ import java.io.File;
 import java.io.IOException;
 import java.util.AbstractMap;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Objects;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.prefs.Preferences;
 import lombok.extern.java.Log;
-import org.usb4java.Context;
 import org.usb4java.DeviceList;
 import org.usb4java.LibUsb;
 import org.usb4java.LibUsbException;
@@ -32,19 +32,19 @@ import sun.security.pkcs11.wrapper.PKCS11Exception;
 @Log
 public class DeviceManager {
 
-    private Properties driversList = new Properties();
+    private final Properties driversList = new Properties();
     private final String key = "Paths";
     private Preferences driverPaths;
-    private static DeviceManager singleton = new DeviceManager();
+    private static final DeviceManager singleton = new DeviceManager();
 
-    private int pluggedUsbCount = 0;
+    //Keeps track of the number of attached USB devices. Updated after each  countAttachedUSBDevices() call.
+    private int attachedUSBCount = Integer.MIN_VALUE;
 
-    private volatile boolean exiting = false;
-    //Javax4usb context
-    static Context context = new Context();
+    //The first string represents the slot description. The entry contains an integer which represents the slotIndex and a File which is the Path to driver.
+    Map<String, Entry<Integer, File>> deviceMap = new HashMap<>();
 
     static {
-        int result = LibUsb.init(context);
+        int result = LibUsb.init(null);
         if (result < 0) {
             throw new LibUsbException("Unable to initialize libusb", result);
         }
@@ -54,7 +54,7 @@ public class DeviceManager {
         return singleton;
     }
 
-    private void initialize() {
+    private void initDriverListAndPaths() {
         driverPaths = Preferences.userNodeForPackage(DeviceManager.class);
         try {
             driversList.load(DeviceManager.class.getResourceAsStream("/" + "DeviceManager.properties"));
@@ -66,15 +66,34 @@ public class DeviceManager {
         }
     }
 
-    //TODO
-    public synchronized Map<String, Map.Entry<Integer, File>> SearchForDevices() throws PKCS11Exception {
-        initialize();
+    public synchronized Map<String, Entry<Integer, File>> scanForUSBDevices() throws PKCS11Exception {
+        int newUSBCount = countAttachedUSBDevices();
+        System.out.println(newUSBCount);
+        if (newUSBCount > attachedUSBCount) {
+            attachedUSBCount = newUSBCount;
+            return scanAllDirectories();
+        } else if (newUSBCount < attachedUSBCount) {
+            attachedUSBCount = newUSBCount;
+            return checkDeviceStatus();
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * Scans all current directories for any known drivers and checks if there's
+     * any device that works with this driver. Used when a new device has been
+     * attached.
+     *
+     * @return List of attached devices
+     * @throws PKCS11Exception
+     */
+    private Map<String, Entry<Integer, File>> scanAllDirectories() throws PKCS11Exception {
+        initDriverListAndPaths();
         String[] paths = driverPaths.get(key, null).split(";", -1);
         String driver;
-        //The first string represents the slot description. The second represents both the slotListIndex and the driver for the token.
-        Map<String, Map.Entry<Integer, File>> tokensDescription = new HashMap<>();
         for (String path : paths) {
-            for (Map.Entry<Object, Object> entry : driversList.entrySet()) {
+            for (Entry<Object, Object> entry : driversList.entrySet()) {
                 driver = path.concat("\\").concat(entry.getValue().toString()).concat(".dll");
                 PKCS11 p11 = null;
                 try {
@@ -82,8 +101,8 @@ public class DeviceManager {
                     long[] slots = p11.C_GetSlotList(true);
                     if (slots.length > 0) {
                         for (int i = 0; i < slots.length; i++) {
-                            Map.Entry<Integer, File> slotIdAndDriver = new AbstractMap.SimpleEntry<>(i, new File(driver));
-                            tokensDescription.put(new String(p11.C_GetSlotInfo(slots[i]).slotDescription).trim(), slotIdAndDriver);
+                            Entry<Integer, File> slotIdAndDriver = new AbstractMap.SimpleEntry<>(i, new File(driver));
+                            deviceMap.put(new String(p11.C_GetSlotInfo(slots[i]).slotDescription).trim(), slotIdAndDriver);
                         }
                     }
                 } catch (IOException ex) {
@@ -91,29 +110,53 @@ public class DeviceManager {
                 }
             }
         }
-        return tokensDescription;
+        return deviceMap;
     }
 
-    public Map<String, Map.Entry<Integer, File>> CheckDeviceStatus(List<Map.Entry<Integer, File>> slotIndexAndDriver) {
+    /**
+     * Checks if a device /identified by both slotIndex and driver/ is still
+     * available. Called when any USB device has been removed to check if its a
+     * token and to eventually remove it from the list of attached devices.
+     *
+     * @return Map containing the updated list of devices.
+     */
+    private Map<String, Entry<Integer, File>> checkDeviceStatus() throws PKCS11Exception {
         PKCS11 p11 = null;
-        for (Map.Entry<Integer, File> deviceProperties : slotIndexAndDriver) {
-            //p11 = PKCS11.getInstance(deviceProperties.getValue(), "C_GetFunctionList", null, false);
+        Entry<String, Entry<Integer, File>> device = null;
+        Map<String, Entry<Integer, File>> newDeviceMap = new HashMap<>();
+        String driver;
+        for (Iterator<Entry<String, Entry<Integer, File>>> iterator = deviceMap.entrySet().iterator(); iterator.hasNext();) {
+            try {
+                device = iterator.next();
+                driver = device.getValue().getValue().toString();
+                p11 = PKCS11.getInstance(driver, "C_GetFunctionList", null, false);
+                long[] slots = p11.C_GetSlotList(true);
+                if (slots.length > 0) {
+                    for (int i = 0; i < slots.length; i++) {
+                        Entry<Integer, File> slotIdAndDriver = new AbstractMap.SimpleEntry<>(i, new File(driver));
+                        newDeviceMap.put(new String(p11.C_GetSlotInfo(slots[i]).slotDescription).trim(), slotIdAndDriver);
+                    }
+                }
+                deviceMap = newDeviceMap;
+            } catch (IOException ex) {
+                deviceMap.remove(device.getKey());
+            }
         }
-        return null;
+        return deviceMap;
     }
 
-    public void scanUsbDevices() {
-        // Read the USB device list
+    /**
+     * Counts all currently attached to the computer USB devices.
+     * @return number of currently attached USB devices.
+     */
+    private int countAttachedUSBDevices() {
         DeviceList list = new DeviceList();
-        int newUsbCount = LibUsb.getDeviceList(context, list);
+        int newUsbCount = LibUsb.getDeviceList(null, list);
+        //Bad idea, but cannot figure out how to call exit from another thread.
         if (newUsbCount < 0) {
             throw new LibUsbException("Unable to get device list", newUsbCount);
         }
-        //TODO
-        //if()
-        if (exiting) {
-            LibUsb.exit(context);
-        }
+        return newUsbCount;
     }
 
     public void AddNewPathToDriver(String path) throws IllegalArgumentException {
