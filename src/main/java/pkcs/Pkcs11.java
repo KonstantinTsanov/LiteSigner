66f9720c71fbdb1e5c0cb9248dce0970af44f90c
Konstantin Tsanov <k.tsanov@gmail.com>
2017-06-15 22:06:01 +0300

    Better exception management.

--- src/main/java/pkcs/Pkcs11.java
+++ src/main/java/pkcs/Pkcs11.java
@@ -7,10 +7,13 @@ package pkcs;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.IOException;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
 import java.security.Security;
 import java.security.cert.Certificate;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -18,6 +21,7 @@ import java.util.List;
 import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.naming.AuthenticationException;
 import javax.security.auth.login.LoginException;
 import lombok.extern.java.Log;
 import sun.security.pkcs11.SunPKCS11;
@@ -28,11 +32,11 @@ import sun.security.pkcs11.SunPKCS11;
  */
 @Log
 public class Pkcs11 extends Pkcs1_ {
-    
+
     private final File _driver;
     private final long _slotId;
     private final String slotDescription;
-    
+
     private static int pluggedCount = 0;
 
     /**
@@ -49,14 +53,14 @@ public class Pkcs11 extends Pkcs1_ {
         _slotId = slotId;
         registerProvider();
     }
-    
+
     private void registerProvider() {
         String pkcs11Config = String.format("name=%s\nlibrary=%s\nslotListIndex=%d", "SmartCard" + pluggedCount++, _driver, _slotId);
         ByteArrayInputStream configStream = new ByteArrayInputStream(pkcs11Config.getBytes());
         _provider = new sun.security.pkcs11.SunPKCS11(configStream);
         Security.addProvider(_provider);
     }
-    
+
     public void closeSession() {
         try {
             ((SunPKCS11) _provider).logout();
@@ -68,70 +72,50 @@ public class Pkcs11 extends Pkcs1_ {
         _provider = null;
         pluggedCount--;
     }
-    
+
     @Override
-    public final void login() throws KeyStoreException {
-        _chp = new KeyStore.CallbackHandlerProtection(_guiHandler);
-        _builder = KeyStore.Builder.newInstance("PKCS11", _provider, _chp);
-        try {
-            _certKeyStore = _builder.getKeyStore();
-        } catch (KeyStoreException ex) {
-            log.log(Level.SEVERE, "Error occured during login!", ex);
-            throw ex;
+    public final void login() throws KeyStoreException, IOException, NoSuchAlgorithmException, CertificateException, AuthenticationException {
+        char[] pin = _guiPasswordCallback.getPassword();
+        if (pin != null) {
+            _certKeyStore = KeyStore.getInstance("PKCS11", _provider);
+            _certKeyStore.load(null, pin);
+        } else {
+            throw new AuthenticationException("No pin input.");
         }
     }
-    
+
     public String getSlotDescription() {
         return slotDescription;
     }
-    
-    public Certificate[] getCertificateChain(String alias) throws KeyStoreException {
-        return _certKeyStore.getCertificateChain(alias);
+
+    public X509Certificate[] getCertificateChain(String alias) throws KeyStoreException {
+        return (X509Certificate[]) _certKeyStore.getCertificateChain(alias);
     }
-    
+
     public String getCertificateAlias(Certificate cert) throws KeyStoreException {
         return _certKeyStore.getCertificateAlias(cert);
     }
-    
-    public List<X509Certificate> listCertificates() {
+
+    public List<X509Certificate> listCertificates() throws KeyStoreException {
         List<X509Certificate> list = new ArrayList<>();
-        try {
-            java.util.Enumeration aliases = _certKeyStore.aliases();
-            while (aliases.hasMoreElements()) {
-                Object alias = aliases.nextElement();
-                try {
-                    X509Certificate cert0 = (X509Certificate) _certKeyStore.getCertificate(alias.toString());
-                    list.add(cert0);
-                } catch (KeyStoreException e) {
-                    log.log(Level.SEVERE, "There's a problem with your keystore!");
-                    throw new RuntimeException("Keystore exception! ", e);
-                }
-            }
-        } catch (Exception e) {
-            //TODO
-            log.log(Level.SEVERE, "There's a problem!");
-            throw new RuntimeException("Keystore! ", e);
+        java.util.Enumeration aliases = _certKeyStore.aliases();
+        while (aliases.hasMoreElements()) {
+            Object alias = aliases.nextElement();
+            System.out.println(alias);
+            X509Certificate cert0 = (X509Certificate) _certKeyStore.getCertificate(alias.toString());
+            list.add(cert0);
         }
         return list;
     }
-    
+
     @Override
-    public List<String> listAliases() {
-        try {
-            return Collections.list(_certKeyStore.aliases());
-        } catch (KeyStoreException ex) {
-            log.log(Level.SEVERE, "The keystore has not been loaded!", ex);
-            throw new RuntimeException("Keystore not loaded!", ex);
-        }
+    public List<String> listAliases() throws KeyStoreException {
+        return Collections.list(_certKeyStore.aliases());
+
     }
-    
+
     @Override
-    public X509Certificate getCertificate(String alias) {
-        try {
-            return (X509Certificate) _certKeyStore.getCertificate(alias);
-        } catch (KeyStoreException ex) {
-            log.log(Level.SEVERE, "The keystore has not been loaded!", ex);
-            throw new RuntimeException("Keystore not loaded!", ex);
-        }
+    public X509Certificate getCertificate(String alias) throws KeyStoreException {
+        return (X509Certificate) _certKeyStore.getCertificate(alias);
     }
 }
