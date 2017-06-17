651da5485e7b19065e8723f46523243463b8c912
Konstantin Tsanov <k.tsanov@gmail.com>
2017-06-17 22:19:02 +0300

    Added signature type enum.

---
+++ src/main/java/enums/SignatureType.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package enums;
+
+/**
+ *
+ * @author Konstantin Tsanov <k.tsanov@gmail.com>
+ */
+public enum SignatureType {
+    Attached,
+    Detached,
+    Pdf
+}
