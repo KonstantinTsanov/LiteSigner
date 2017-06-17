15db3c0455a7f2fc2ad4ed4a4380a4f12dd26022
Konstantin Tsanov <k.tsanov@gmail.com>
2017-06-17 14:18:44 +0300

    Renamed.

---
+++ src/main/java/callbacks/PasswordCallback.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package callbacks;
+
+/**
+ *
+ * @author Konstantin Tsanov <k.tsanov@gmail.com>
+ */
+public interface PasswordCallback {
+
+    public char[] getPassword();
+}
