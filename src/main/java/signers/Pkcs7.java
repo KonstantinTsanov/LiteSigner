c22f4860aed10459e96ab370658f42e290f73d3d
Konstantin Tsanov <k.tsanov@gmail.com>
2017-06-18 20:18:20 +0300

    Added signing and timestamp implementations.

--- src/main/java/signers/Pkcs7.java
+++ src/main/java/signers/Pkcs7.java
@@ -5,35 +5,49 @@
  */
 package signers;
 
+import exceptions.TimestampingException;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.security.MessageDigest;
 import java.security.PrivateKey;
 import java.security.Security;
-import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateEncodingException;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
 import java.util.logging.Level;
-import java.util.logging.Logger;
-import javax.security.auth.callback.Callback;
-import javax.security.auth.callback.PasswordCallback;
 import lombok.extern.java.Log;
+import org.bouncycastle.asn1.ASN1EncodableVector;
+import org.bouncycastle.asn1.ASN1InputStream;
+import org.bouncycastle.asn1.ASN1Primitive;
+import org.bouncycastle.asn1.DERSet;
+import org.bouncycastle.asn1.cms.Attribute;
+import org.bouncycastle.asn1.cms.AttributeTable;
+import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
+import org.bouncycastle.asn1.tsp.TimeStampResp;
 import org.bouncycastle.cert.jcajce.JcaCertStore;
-import org.bouncycastle.cms.CMSException;
 import org.bouncycastle.cms.CMSProcessableFile;
 import org.bouncycastle.cms.CMSSignedData;
 import org.bouncycastle.cms.CMSSignedDataGenerator;
+import org.bouncycastle.cms.SignerInformation;
+import org.bouncycastle.cms.SignerInformationStore;
 import org.bouncycastle.cms.jcajce.JcaSignerInfoGeneratorBuilder;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.operator.ContentSigner;
-import org.bouncycastle.operator.OperatorCreationException;
 import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.bouncycastle.operator.jcajce.JcaDigestCalculatorProviderBuilder;
+import org.bouncycastle.tsp.TSPAlgorithms;
+import org.bouncycastle.tsp.TimeStampRequest;
+import org.bouncycastle.tsp.TimeStampRequestGenerator;
+import org.bouncycastle.tsp.TimeStampResponse;
+import org.bouncycastle.tsp.TimeStampToken;
 import org.bouncycastle.util.Store;
 import pkcs.Pkcs1_;
 
@@ -48,17 +62,18 @@ public class Pkcs7 extends Signer {
     private final File _output;
     private final String _alias;
     private X509Certificate _cert;
-    Callback[] callbacks = {new PasswordCallback("password", false)};
+    private URL _timestampAddress;
 
-    public Pkcs7(Pkcs1_ pkcs1x, String alias, File input, File output) {
+    public Pkcs7(Pkcs1_ pkcs1x, String alias, File input, File output, URL timestampAddress) {
         _pkcs1x = pkcs1x;
         _alias = alias;
         _input = input;
         _output = output;
+        _timestampAddress = timestampAddress;
     }
 
     @Override
-    public void sign() {
+    public void sign(boolean attached) {
         try {
             Objects.requireNonNull(_input);
             Objects.requireNonNull(_output);
@@ -69,7 +84,7 @@ public class Pkcs7 extends Signer {
             certList.add(cert);
             Store certs = new JcaCertStore(certList);
             CMSProcessableFile inputFile = new CMSProcessableFile(_input);
-            char[] pin = _pkcs1x.get_guiPasswordCallback().getPassword();
+            char[] pin = _pkcs1x.get_passwordCallback().getPin();
             ContentSigner sha1Signer = new JcaContentSignerBuilder("SHA1withRSA")
                     .setProvider(_pkcs1x.get_provider()).build((PrivateKey) _pkcs1x.get_certKeyStore()
                     .getKey(_alias, pin));
@@ -80,28 +95,70 @@ public class Pkcs7 extends Signer {
 
             gen.addCertificates(certs);
 
-            CMSSignedData sigData = gen.generate(inputFile, false);
-
+            CMSSignedData sigData = gen.generate(inputFile, attached);
+            //Timestamping
+            if (_timestampAddress != null) {
+                CMSSignedData signedTimestampData = new CMSSignedData(sigData.toASN1Structure());
+                Collection<SignerInformation> ss = signedTimestampData.getSignerInfos().getSigners();
+                SignerInformation si = ss.iterator().next();
+                ASN1EncodableVector timestampVector = new ASN1EncodableVector();
+                MessageDigest md = MessageDigest.getInstance("SHA1", "BC");
+                TimeStampResponse response = timestampRequest(md.digest(si.getSignature()), _timestampAddress.toURI());
+                TimeStampToken timestampToken = response.getTimeStampToken();
+                Attribute a = new Attribute(PKCSObjectIdentifiers.id_aa_signatureTimeStampToken, new DERSet(ASN1Primitive.fromByteArray(timestampToken.getEncoded())));
+                timestampVector.add(a);
+                AttributeTable at = new AttributeTable(timestampVector);
+                si = SignerInformation.replaceUnsignedAttributes(si, at);
+                ss.clear();
+                ss.add(si);
+                SignerInformationStore newSignerStore = new SignerInformationStore(ss);
+                sigData = CMSSignedData.replaceSigners(signedTimestampData, newSignerStore);
+            }
             try (FileOutputStream fileOuputStream = new FileOutputStream(_output)) {
                 fileOuputStream.write(sigData.getEncoded());
                 fileOuputStream.flush();
             }
 //TODO
-        } catch (CertificateEncodingException ex) {
-            Logger.getLogger(Pkcs7.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (KeyStoreException ex) {
-            log.log(Level.SEVERE, "The keystore hasnt been loaded!", ex);
-            throw new RuntimeException("The keystore hasnt been loaded!", ex);
-        } catch (CMSException ex) {
-            Logger.getLogger(Pkcs7.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(Pkcs7.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (NoSuchAlgorithmException ex) {
-            Logger.getLogger(Pkcs7.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (UnrecoverableKeyException ex) {
-            Logger.getLogger(Pkcs7.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (OperatorCreationException ex) {
-            Logger.getLogger(Pkcs7.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            log.log(Level.SEVERE, "Failed to sign!", ex);
+            ex.printStackTrace(System.out);
+        }
+    }
+
+    private static TimeStampResponse timestampRequest(byte[] data, URI uri) throws TimestampingException {
+        try {
+            TimeStampRequestGenerator reqgen = new TimeStampRequestGenerator();
+            reqgen.setCertReq(true);
+            TimeStampRequest req = reqgen.generate(TSPAlgorithms.SHA1, data);
+            byte request[] = req.getEncoded();
+            HttpURLConnection conn = (HttpURLConnection) uri.toURL().openConnection();
+            conn.setConnectTimeout(10000);
+            conn.setReadTimeout(10000);
+            conn.setDoOutput(true);
+            conn.setDoInput(true);
+            //conn.setUseCaches(false);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-type", "application/timestamp-query");
+            conn.setRequestProperty("Content-length", String.valueOf(request.length));
+            try (OutputStream out = conn.getOutputStream()) {
+                out.write(request);
+                out.flush();
+            } catch (Exception ex) {
+                throw new RuntimeException(ex);
+            }
+            if (conn.getResponseCode() >= 400) {
+                throw new IOException("Unable to complete the timestamping due to HTTP error: " + conn.getResponseCode() + " - " + conn.getResponseMessage());
+            }
+            InputStream in = conn.getInputStream();
+            TimeStampResp resp = TimeStampResp.getInstance(new ASN1InputStream(in).readObject());
+            TimeStampResponse response = new TimeStampResponse(resp);
+            response.validate(req);
+            if (response.getStatus() != 0) {
+                throw new IOException("Unable to complete the timestamping due to an invalid response (" + response.getStatusString() + ")");
+            }
+            return response;
+        } catch (Exception ex) {
+            throw new TimestampingException("Unable to complete the timestamping", ex);
         }
     }
 
