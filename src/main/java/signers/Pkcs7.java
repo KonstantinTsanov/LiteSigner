b5e9b8a17f5cc20db4937b370005aca71bac83d6
Konstantin Tsanov <k.tsanov@gmail.com>
2017-06-19 23:48:56 +0300

    Added sign and verify implementations for attached and detached signatures.

--- src/main/java/signers/Pkcs7.java
+++ src/main/java/signers/Pkcs7.java
@@ -5,8 +5,12 @@
  */
 package signers;
 
+import exceptions.CertificateVerificationException;
+import exceptions.SignatureValidationException;
 import exceptions.TimestampingException;
+import java.io.DataInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -17,13 +21,18 @@ import java.net.URL;
 import java.security.MessageDigest;
 import java.security.PrivateKey;
 import java.security.Security;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Locale;
 import java.util.Objects;
+import java.util.ResourceBundle;
 import java.util.logging.Level;
 import lombok.extern.java.Log;
+import org.bouncycastle.asn1.ASN1Encodable;
 import org.bouncycastle.asn1.ASN1EncodableVector;
 import org.bouncycastle.asn1.ASN1InputStream;
 import org.bouncycastle.asn1.ASN1Primitive;
@@ -32,24 +41,41 @@ import org.bouncycastle.asn1.cms.Attribute;
 import org.bouncycastle.asn1.cms.AttributeTable;
 import org.bouncycastle.asn1.pkcs.PKCSObjectIdentifiers;
 import org.bouncycastle.asn1.tsp.TimeStampResp;
+import org.bouncycastle.cert.X509CertificateHolder;
 import org.bouncycastle.cert.jcajce.JcaCertStore;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.cms.CMSException;
+import org.bouncycastle.cms.CMSProcessableByteArray;
 import org.bouncycastle.cms.CMSProcessableFile;
 import org.bouncycastle.cms.CMSSignedData;
 import org.bouncycastle.cms.CMSSignedDataGenerator;
+import org.bouncycastle.cms.DefaultCMSSignatureAlgorithmNameGenerator;
+import org.bouncycastle.cms.SignerId;
 import org.bouncycastle.cms.SignerInformation;
 import org.bouncycastle.cms.SignerInformationStore;
+import org.bouncycastle.cms.SignerInformationVerifier;
+import org.bouncycastle.cms.bc.BcRSASignerInfoVerifierBuilder;
 import org.bouncycastle.cms.jcajce.JcaSignerInfoGeneratorBuilder;
+import org.bouncycastle.cms.jcajce.JcaSimpleSignerInfoVerifierBuilder;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.DefaultDigestAlgorithmIdentifierFinder;
+import org.bouncycastle.operator.DefaultSignatureAlgorithmIdentifierFinder;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.bc.BcDigestCalculatorProvider;
 import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.bouncycastle.operator.jcajce.JcaDigestCalculatorProviderBuilder;
 import org.bouncycastle.tsp.TSPAlgorithms;
+import org.bouncycastle.tsp.TSPException;
 import org.bouncycastle.tsp.TimeStampRequest;
 import org.bouncycastle.tsp.TimeStampRequestGenerator;
 import org.bouncycastle.tsp.TimeStampResponse;
 import org.bouncycastle.tsp.TimeStampToken;
+import org.bouncycastle.tsp.TimeStampTokenInfo;
 import org.bouncycastle.util.Store;
+import org.bouncycastle.util.StoreException;
 import pkcs.Pkcs1_;
+import tools.CertificateVerifier;
 
 /**
  *
@@ -58,13 +84,18 @@ import pkcs.Pkcs1_;
 @Log
 public class Pkcs7 extends Signer {
 
-    private final File _input;
-    private final File _output;
-    private final String _alias;
-    private X509Certificate _cert;
+    private File _input;
+    private File _output;
+    private String _alias;
     private URL _timestampAddress;
+    private Locale locale;
 
-    public Pkcs7(Pkcs1_ pkcs1x, String alias, File input, File output, URL timestampAddress) {
+    public Pkcs7(Locale locale) {
+        this.locale = locale;
+    }
+
+    public Pkcs7(Pkcs1_ pkcs1x, String alias, File input, File output, URL timestampAddress, Locale locale) {
+        this.locale = locale;
         _pkcs1x = pkcs1x;
         _alias = alias;
         _input = input;
@@ -121,7 +152,7 @@ public class Pkcs7 extends Signer {
 //TODO
         } catch (Exception ex) {
             log.log(Level.SEVERE, "Failed to sign!", ex);
-            ex.printStackTrace(System.out);
+            //TODO
         }
     }
 
@@ -163,7 +194,158 @@ public class Pkcs7 extends Signer {
     }
 
     @Override
-    public void verify() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public String validate(File pkcs7, File signedFile) throws IOException, SignatureValidationException {
+        if (signedFile == null) {
+            try (InputStream is = new FileInputStream(pkcs7)) {
+                return validateAttachedSignature(is);
+            } catch (CertificateVerificationException ex) {
+                return validateDetachedSignature(pkcs7, signedFile);
+            }
+        } else {
+            return validateDetachedSignature(pkcs7, signedFile);
+        }
+    }
+
+    private String validateAttachedSignature(InputStream attached) throws CertificateVerificationException {
+        String validationResult = "";
+        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+        try {
+            CMSSignedData cmsSignedData = new CMSSignedData(attached);
+            SignerInformationStore signers = cmsSignedData.getSignerInfos();
+            Collection<SignerInformation> c = signers.getSigners();
+            boolean result = false;
+            for (SignerInformation signer : c) {
+                SignerId signerId = signer.getSID();
+                Collection certCollection = cmsSignedData.getCertificates().getMatches(signerId);
+
+                Iterator certIt = certCollection.iterator();
+                X509Certificate cert = new JcaX509CertificateConverter().setProvider("BC").getCertificate((X509CertificateHolder) certIt.next());
+                result = signer.verify(new JcaSimpleSignerInfoVerifierBuilder().setProvider("BC").build(cert));
+                if (!result) {
+                    validationResult = "The signature is not valid!";
+                    return validationResult;
+                }
+                CertificateVerifier.getInstance().validateCertificate(cert);
+                validationResult = validationResult.concat("Certificate validation passed. \nCertificate is valid.");
+                //TIMESTAMP PART
+                AttributeTable unsignedAttributes = signer.getUnsignedAttributes();
+                if (unsignedAttributes != null) {
+                    Attribute timestampAttribute = unsignedAttributes.get(PKCSObjectIdentifiers.id_aa_signatureTimeStampToken);
+                    if (timestampAttribute == null) {
+                        validationResult = validationResult.concat("\nThe signature contains no timestamp");
+                        log.log(Level.FINE, "The signature contains no timestamp.");
+                    } else {
+                        ASN1Encodable dob = timestampAttribute.getAttrValues().getObjectAt(0);
+                        CMSSignedData signedData = new CMSSignedData(dob.toASN1Primitive().getEncoded());
+                        TimeStampToken tst = new TimeStampToken(signedData);
+                        Collection tstSigners = tst.toCMSSignedData().getSignerInfos().getSigners();
+                        TimeStampTokenInfo timestampInfo = validateTimeStamp(tstSigners, tst);
+                        validationResult = validationResult.concat("\nThe signature contains a timestamp.");
+                        validationResult = validationResult.concat(String.format("\nTSA: %s\n Time: %s", timestampInfo.getTsa().toString(), timestampInfo.getGenTime().toString()));
+                    }
+                } else {
+                    validationResult = validationResult.concat("\nThe signature contains no timestamp.");
+                }
+            }
+            return validationResult;
+        } catch (CertificateVerificationException ex) {
+            throw ex;
+        } catch (IOException | CertificateException | CMSException | OperatorCreationException | TSPException | StoreException ex) {
+            log.log(Level.SEVERE, "An error occured while validating the signature!", ex);
+            ResourceBundle r = ResourceBundle.getBundle("CoreBundle", locale);
+            throw new CertificateVerificationException(r.getString("signatureValidationFailedError"), ex);
+        }
+    }
+
+    private String validateDetachedSignature(File pkcs7, File signedFile) throws SignatureValidationException {
+        String validationResult = "";
+        Security.addProvider(new BouncyCastleProvider());
+        try {
+            byte[] Sig_Bytes = new byte[(int) pkcs7.length()];
+            try (DataInputStream in = new DataInputStream(new FileInputStream(pkcs7))) {
+                in.readFully(Sig_Bytes);
+                in.close();
+            }
+            byte[] Data_Bytes = new byte[(int) signedFile.length()];
+            try (DataInputStream input = new DataInputStream(new FileInputStream(signedFile))) {
+                input.readFully(Data_Bytes);
+                input.close();
+            }
+            try {
+                CMSSignedData cms = new CMSSignedData(new CMSProcessableByteArray(Data_Bytes), Sig_Bytes);
+                Store certs = cms.getCertificates();
+                SignerInformationStore signers = cms.getSignerInfos();
+                Iterator it = signers.getSigners().iterator();
+                if (it.hasNext()) {
+                    SignerInformation signer = (SignerInformation) it.next();
+                    X509CertificateHolder cert = (X509CertificateHolder) certs.getMatches(signer.getSID()).iterator().next();
+
+                    SignerInformationVerifier verifier = new BcRSASignerInfoVerifierBuilder(
+                            new DefaultCMSSignatureAlgorithmNameGenerator(),
+                            new DefaultSignatureAlgorithmIdentifierFinder(),
+                            new DefaultDigestAlgorithmIdentifierFinder(),
+                            new BcDigestCalculatorProvider()).build(cert);
+                    byte[] data = (byte[]) cms.getSignedContent().getContent();
+                    if (signer.verify(verifier)) {
+                        validationResult = "Signature verified sucessfully.";
+                    } else {
+                        validationResult = "Signature verification failed!";
+                        return validationResult;
+                    }
+                    AttributeTable unsignedAttributes = signer.getUnsignedAttributes();
+                    if (unsignedAttributes != null) {
+                        Attribute timestampAttribute = unsignedAttributes.get(PKCSObjectIdentifiers.id_aa_signatureTimeStampToken);
+                        if (timestampAttribute == null) {
+                            validationResult = validationResult.concat("\nThe signature contains no timestamp");
+                            log.log(Level.FINE, "The signature contains no timestamp.");
+                        } else {
+                            ASN1Encodable dob = timestampAttribute.getAttrValues().getObjectAt(0);
+                            CMSSignedData signedData = new CMSSignedData(dob.toASN1Primitive().getEncoded());
+                            TimeStampToken tst = new TimeStampToken(signedData);
+                            Collection tstSigners = tst.toCMSSignedData().getSignerInfos().getSigners();
+                            TimeStampTokenInfo timestampInfo = validateTimeStamp(tstSigners, tst);
+                            validationResult = validationResult.concat("\nThe signature contains a timestamp.");
+                            validationResult = validationResult.concat(String.format("\nTSA: %s\n Time: %s", timestampInfo.getTsa().toString(), timestampInfo.getGenTime().toString()));
+                        }
+                    } else {
+                        validationResult = validationResult.concat("\nThe signature contains no timestamp.");
+                    }
+                }
+
+            } catch (Exception e) {
+                throw new SignatureValidationException("Could not validate signature!");
+            }
+            return validationResult;
+        } catch (IOException ex) {
+            throw new SignatureValidationException("Cannot read files!");
+        }
+    }
+
+    /**
+     * Validates timestamp
+     */
+    private TimeStampTokenInfo validateTimeStamp(Collection timestampSigners, TimeStampToken tsToken) throws CertificateVerificationException {
+        try {
+            Iterator tstIt = timestampSigners.iterator();
+            while (tstIt.hasNext()) {
+                SignerInformation tstSignerInformation = (SignerInformation) tstIt.next();
+                Store tstCerts = tsToken.getCertificates();
+                Iterator tstcertIt = tstCerts.getMatches(tstSignerInformation.getSID()).iterator();
+                X509CertificateHolder tstCert = (X509CertificateHolder) tstcertIt.next();
+                if (tstcertIt.hasNext()) {
+                    throw new Exception("Expected exactly one certificate for each signer in the timestamp.");
+                }
+                SignerInformationVerifier verifier = new BcRSASignerInfoVerifierBuilder(
+                        new DefaultCMSSignatureAlgorithmNameGenerator(),
+                        new DefaultSignatureAlgorithmIdentifierFinder(),
+                        new DefaultDigestAlgorithmIdentifierFinder(),
+                        new BcDigestCalculatorProvider()).build(tstCert);
+                tsToken.validate(verifier);
+            }
+            return tsToken.getTimeStampInfo();
+        } catch (Exception ex) {
+            log.log(Level.SEVERE, "Error occured while validating timestamp!", ex);
+            throw new CertificateVerificationException("Could not validate timestamp..");
+        }
     }
 }
